8f45d246324a45b37d28c865b1ff980f74b56642
Jacob Chacko <philosopherjake@msn.com>
2025-05-07 09:54:21 -0600

    Add project files.

---
+++ SimpleServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Add your server logic here
+        }
+    }
